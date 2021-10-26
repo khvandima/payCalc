0321219e9a66d1aeeaaa7daedeff2b59f9a36576
KHVAN DMITRIY <khvandima@gmail.com>
2021-10-27 05:24:47 +0900

    create Monitor component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,13 @@ import Monitor from '../Monitor/Monitor.js';
 import CalendarGrid from '../CalendarGrid/CalendarGrid.js';
 
 const ShadowWrapper = styled.div`
+  border-top: 1px solid #737374;
+  border-right: 1px solid #464648;
+  border-bottom: 1px solid #464648;
+  border-left: 1px solid #464648;
   border-radius: 8px;
   overflow: hidden;
+  box-shadow: 0 0 0 1px #1A1A1A, 0 8px 20px 6px #888;
 `;
 
 function App() {
